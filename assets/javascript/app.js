6759ca419fa4606e86d42b9f92394e3d940f8e31
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-08-26 20:51:38 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+// Create variables for all html elements
+// Time Remaining
+// Question
+// Answer Space 1
+// Answer Space 2
+// Answer Space 3
+// Answer Space 4
+// Correct
+// Incorrect
+// Gif
+// Game End Text
+// Correct Answer Count
+// Incorrect Answer Count
+// Unanswered Count
+// Start Over Button
+
+
+
+// Objects for each question
+// Consisting of keys: question, correct answer, incorrect answers, image
+
+// Randomize order of answer array to insert into answer spaces
+
+// Create buttons for each answer space
+
+// Add button hovers
+
+// Create timer to countdown 30 seconds for each question
+
+// If player selects correct answer, increase wins, stop timer, clear question and show correct image and answer text
+
+// If player selects incorrect answer, increase losses, stop timer, clear question and show correct image and answer text
+
+// If player does not select answer, increase unanswered count, stop timer, clear question and show correct image and answer text
+
+// After a question, wait a few seconds and go to the next question
