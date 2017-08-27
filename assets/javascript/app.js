3d7dffd73eeb4c718784484c51c9922061f06fd2
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-08-27 17:25:10 -0500

    testing trivia content defined as objects in an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,90 +1,107 @@
-// Create variables for all html elements
+$(document).ready(function() {
 
-// Time Remaining
-var timeRemaining = $("#timeRemaining");
-timeRemaining.html("<h2> Time Remaining: </h2>");
+	// Create variables for all html elements
 
-// Question
-var currentQuestion = $("#currentQuestion");
+	// Time Remaining
+	var timeRemaining = $("#timeRemaining");
+	timeRemaining.html("<h2> Time Remaining: " + countdown + "</h2>");
 
-// Answer Choices
-var answerChoices = $("#answerChoices");
+	// Question
+	var currentQuestion = $("#currentQuestion");
 
-// Result
-var result = $("#result");
+	// Answer Choices
+	var answerChoices = $("#answerChoices");
 
-// Correct Answer
-var correctAnswer = $("#correctAnswer");
+	// Result
+	var result = $("#result");
 
-// Gif
-var gif = $("#gif");
+	// Correct Answer
+	var correctAnswer = $("#correctAnswer");
 
-// Correct Answer Count
-var correctCount = $("#correctCount");
-var correct = 0;
-correctCount.html("<h3> Correct: " + correct + "</h3>");
+	// Gif
+	var gif = $("#gif");
 
-// Incorrect Answer Count
-var incorrectCount = $("#incorrectCount");
-var incorrect = 0;
-incorrectCount.html("<h3> Incorrect: " + incorrect + "</h3>");
+	// Correct Answer Count
+	var correctCount = $("#correctCount");
+	var correct = 0;
+	// correctCount.html("<h3> Correct: " + correct + "</h3>");
 
-// Unanswered Count
-var unansweredCount = $("#unansweredCount");
-var unanswered = 0;
-unansweredCount.html("<h3> Unanswered: " + unanswered + "</h3>");
+	// Incorrect Answer Count
+	var incorrectCount = $("#incorrectCount");
+	var incorrect = 0;
+	// incorrectCount.html("<h3> Incorrect: " + incorrect + "</h3>");
 
-// Start Over Button
-var restart = $("#restart");
+	// Unanswered Count
+	var unansweredCount = $("#unansweredCount");
+	var unanswered = 0;
+	// unansweredCount.html("<h3> Unanswered: " + unanswered + "</h3>");
 
+	// Start Over Button
+	var restart = $("#restart");
 
-// Objects for each question
-// Consisting of keys: question, correct answer, incorrect answers, image
+	// Declaring interval ID to store countdown ID
+	var intervalId;
 
-var question1 = {
-	question: "What is Rachel Green's middle name?",
-	correctAnswer: "Karen",
-	incorrectAnswers: ["Monica", "Anne", "Bertha"],
-	answerChoices: ["Monica", "Anne", "Bertha", "Karen"],
-	gifURL: "rachel-karen-green.gif"
-}; 
+	// Countdown Timer
+	var countdown = 30;
 
-var question2 = {
-	question: "Name the dog in Full House",
-	correctAnswer: "Coment",
-	incorrectAnswers: ["Flash", "Luna", "Apollo"],
-	answerChoices: ["Flash", "Comet", "Luna", "Apollo"],
-	gifURL: "comet-dog.gif"
-}; 
-
-triviaQuestions = [question1, question2];
-
-triviaQuestions.forEach(function(currentElement) {
-	currentQuestion.html(currentElement.question);
-
-	currentElement.answerChoices.forEach(function(choice) {
-	var button = $("<button>");
-	button.text(choice);
-	var newDiv = $("<div>");
-	newDiv.append(button);
-	answerChoices.append(newDiv);
-});
-
-});
+	var countdownTimer;
+	var thirtySeconds;
+	var stop;
 
 
-// Randomize order of answer array to insert into answer spaces
+	// Objects for each question
+	// Consisting of keys: question, correct answer, incorrect answers, image
 
-// Create buttons for each answer space
+	var question1 = {
+		question: "What is Rachel Green's middle name?",
+		answerChoices: ["Monica", "Maryanne", "Karen", "Kimberlina"],
+		answerIndex: 2,
+		answer: "Karen"
+	}
 
-// Add button hovers
+	function displayQuestion(q) {
 
-// Create timer to countdown 30 seconds for each question
+		countdownTimer();
+		timeRemaining.html("<h2> Time Remaining: " + countdown + "</h2>");
+		currentQuestion.html(q.question);
+	}
 
-// If player selects correct answer, increase wins, stop timer, clear question and show correct image and answer text
+	displayQuestion(question1);
 
-// If player selects incorrect answer, increase losses, stop timer, clear question and show correct image and answer text
 
-// If player does not select answer, increase unanswered count, stop timer, clear question and show correct image and answer text
+	// Randomize order of answer array to insert into answer spaces
 
-// After a question, wait a few seconds and go to the next question
+	// Create buttons for each answer space
+
+	// Add button hovers
+
+	// --------------------------- Function to Countdown 30 Seconds then Stop ------------------------------//
+	function countdownTimer() {
+		intervalId = setInterval(thirtySeconds, 1000);
+	};
+
+	function thirtySeconds() {
+		countdown--;
+		timeRemaining.html("<h2> Time Remaining: " + countdown + "</h2>");
+
+		if (countdown === 0) {
+			stop();
+		} 
+	}
+
+	function stop() {
+		clearInterval(intervalId);
+	}
+	// ------------------------------------------------------------------------------------------------------//
+
+
+	// If player selects correct answer, increase wins, stop timer, clear question and show correct image and answer text
+
+	// If player selects incorrect answer, increase losses, stop timer, clear question and show correct image and answer text
+
+	// If player does not select answer, increase unanswered count, stop timer, clear question and show correct image and answer text
+
+	// After a question, wait a few seconds and go to the next question
+
+})
---
+++ assets/javascript/basicApp.js
@@ -0,0 +1,99 @@
+var mainContent = $("#mainContent");
+var countdown = 2;
+var intervalId;
+var timeRemaining;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+// --------------------------- Function to Countdown 120 Seconds then Stop ------------------------------//
+	function countdownTimer() {
+		intervalId = setInterval(twoMinutes, 1000);
+	};
+
+	function twoMinutes() {
+		countdown--;
+		// timeRemaining.html("<h2> Time Remaining: " + countdown + "</h2>");
+
+		if (countdown === 0) {
+			finalScreen();
+		} 
+	}
+
+	function finalScreen() {
+		clearInterval(intervalId);
+
+	}
+
+	countdownTimer();
+
+// ------------------------- Questions and Answers -----------------------------------------------------//
+var triviaQuestions = [
+
+	question1 = {
+		question: "Test question 1",
+		answerChoices: ["correct answer", "incorrect 1", "incorrect 2", "incorrect 3"],
+		correctAnswerIndex: 0
+	}, 
+
+	question2 = {
+		question: "Test question 2",
+		answerChoices: ["incorrect 1", "correct answer", "incorrect 2", "incorrect 3"],
+		correctAnswerIndex: 1
+	}, 
+
+	question3 = {
+		question: "Test question 3",
+		answerChoices: ["incorrect 1", "incorrect 2", "correct answer", "incorrect 3"],
+		correctAnswerIndex: 2
+	}, 
+
+	question4 = {
+		question: "Test question 4",
+		answerChoices: ["incorrect 1", "incorrect 2", "incorrect 3", "correct answer"],
+		correctAnswerIndex: 3
+	}, 
+
+	question5 = {
+		question: "Test question 5",
+		answerChoices: ["correct answer", "incorrect 1", "incorrect 2", "incorrect 3"],
+		correctAnswerIndex: 0
+	}, 
+
+	question6 = {
+		question: "Test question 6",
+		answerChoices: ["incorrect 1", "correct answer", "incorrect 2", "incorrect 3"],
+		correctAnswerIndex: 1
+	}, 
+
+	question7 = {
+		question: "Test question 7",
+		answerChoices: ["incorrect 1", "incorrect 2", "correct answer", "incorrect 3"],
+		correctAnswerIndex: 2
+	}, 
+
+	question8 = {
+		question: "Test question 8",
+		answerChoices: ["incorrect 1", "incorrect 2", "incorrect 3", "correct answer"],
+		correctAnswerIndex: 3
+	}, 
+
+]
+
+function addQuestion() {
+	$("#question1").html(triviaQuestions[0].question);
+	var answerBtns = $("<input type='radio'>")
+	answerBtns.triviaQuestions[0].answerChoices.appendTo($("answers1"));
+}
+
+addQuestion();
+
+// Display all questions 
+// Create buttons for all answer choices
+
+// Display all answer choices beneath question
+// If user click === answerchoices[correctAnswerIndex], correctAnswers++
+// If user click !== answerchoices[correctAnswerIndex], incorrectAnswers++
+// If no user click, unanswered++
+// Disable multi-select answers
+// Add timer to top of page
